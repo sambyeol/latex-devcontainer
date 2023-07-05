38d3972697040ac66624774acddb85d4987f7967
Seokhyun Lee <gbvrcx@gmail.com>
2023-07-05 18:15:56 +0900

    refactor: remove omz in nonroot script

--- script-library/debian-nonroot.sh
+++ script-library/debian-nonroot.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 USERNAME=${1:-"root"}
-USE_OMZ=${2:-"true"}
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e "Script must be run as root."
@@ -13,10 +12,4 @@ if ! [ "${USERNAME}" = "root" ]; then
     useradd --shell $(which zsh) -G sudo -m -d /home/${USERNAME} -k /etc/skel ${USERNAME}
     sed -i -e 's/%sudo.*/%sudo\tALL=(ALL:ALL)\tNOPASSWD:ALL/g' /etc/sudoers
     su ${USERNAME} -s /bin/sh -c "touch /home/${USERNAME}/.sudo_as_admin_successful"
-fi
-
-# Install OMZ
-if [ "${USE_OMZ}" = "true" ] ; then
-    RUNZSH=no su ${USERNAME} -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    su ${USERNAME} -s /bin/sh -c 'sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"simple\"/g" ~/.zshrc'
 fi
